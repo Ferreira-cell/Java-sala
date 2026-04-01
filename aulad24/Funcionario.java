c4a5349b3972fa46c8167872a24ba4f658b9b505
Ferreira-cell <vtinaugusto17@gmail.com>
2026-03-31 21:02:22 -0300

    Add files via upload

---
+++ aulad24/Funcionario.java
@@ -0,0 +1,18 @@
+package com.mycompany.mavenproject1;
+
+
+public class Funcionario extends Pessoa {
+    private double salario;
+    
+    public Funcionario(String nome, int idade, double salario){
+        super(nome, idade);
+        this.salario = salario;    
+    }
+    
+    @Override
+    public void mostrarDados(){
+        super.mostrarDados();
+        System.out.println(" Salario: "+ salario);
+    }
+    
+}
